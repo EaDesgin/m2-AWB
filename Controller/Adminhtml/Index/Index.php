8722cf49a8e0bee4c060411b850aa527bcefd6d4
Gabriela Dohotariu <gaby_m89@yahoo.com>
2018-02-14 12:03:11 +0200

    #2027:Adaugare module.xml,modul eacore,index.php

---
+++ Controller/Adminhtml/Index/Index.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Eadesigndev\AWB\Controller\Adminhtml\Index;
+
+use Magento\Backend\App\Action\Context;
+use Magento\Framework\View\Result\PageFactory;
+
+
+class Index extends \Magento\Backend\App\Action
+{
+    const ADMIN_RESOURCE_VIEW = 'Eadesigndev_AWB::index';
+
+
+    /**
+     * Authorization level of a basic admin session
+     *
+     * @see _isAllowed()
+     */
+    const ADMIN_RESOURCE = 'Magento_Cms::page';
+
+    /**
+     * @var PageFactory
+     */
+    protected $resultPageFactory;
+
+    /**
+     * @param Context $context
+     * @param PageFactory $resultPageFactory
+     */
+    public function __construct(
+        Context $context,
+        PageFactory $resultPageFactory
+    ) {
+        parent::__construct($context);
+        $this->resultPageFactory = $resultPageFactory;
+    }
+
+    /**
+     * Index action
+     *
+     * @return \Magento\Backend\Model\View\Result\Page
+     */
+    public function execute()
+    {
+        /** @var \Magento\Backend\Model\View\Result\Page $resultPage */
+        $resultPage = $this->resultPageFactory->create();
+        $resultPage->setActiveMenu('Magento_Cms::cms_page');
+        $resultPage->addBreadcrumb(__('CMS'), __('CMS'));
+        $resultPage->addBreadcrumb(__('Manage Lista AWB'), __('Manage Lista AWB'));
+        $resultPage->getConfig()->getTitle()->prepend(__('Lista AWB'));
+
+        $dataPersistor = $this->_objectManager->get(\Magento\Framework\App\Request\DataPersistorInterface::class);
+        $dataPersistor->clear('cms_page');
+
+        return $resultPage;
+    }
+}
